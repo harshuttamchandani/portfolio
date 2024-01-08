d1ffc2d9749920677ae686ac2e17cd8e88ac3e9c
Harsh Uttamchandani <harshuttamchandani2002@gmail.com>
2024-01-08 18:20:48 +0530

    side bar update

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    var hamburger = document.querySelector('.hamburger-menu');
+    var navbar = document.getElementById("navbar");
+
+    hamburger.addEventListener('click', function() {
+        navbar.classList.toggle('open');
+        // You should not need to manually set the style if the CSS is correct
     });
 });
+</script>
